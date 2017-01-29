d18b47bfd19d0f12494c99a26032719634ca40f5
Elias Kotlyar <elias.kotlyar@gmail.com>
2017-01-29 18:56:38 +0100

    First commit

---
+++ scripts/phpmyadminautologin.php
@@ -0,0 +1,7 @@
+<?php
+$i = 1;
+$cfg['Servers'][$i]['auth_type'] = 'config';
+$cfg['Servers'][$i]['user'] = 'database';
+$cfg['Servers'][$i]['password'] = 'database';
+$cfg['Servers'][$i]['host'] = 'localhost';
+$cfg['AllowThirdPartyFraming'] = true;
